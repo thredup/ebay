939e82bf5255f406f14b9b258f938cdd38de83d4
eugenebolshakov <eugene.bolshakov@gmail.com>
2011-08-04 17:29:27 +0400

    Support multiple PictureURLs

--- lib/ebay/types/picture_details.rb
+++ lib/ebay/types/picture_details.rb
@@ -17,7 +17,7 @@ module Ebay # :nodoc:
       text_node :gallery_type, 'GalleryType', :optional => true
       text_node :gallery_url, 'GalleryURL', :optional => true
       text_node :photo_display, 'PhotoDisplay', :optional => true
-      text_node :picture_url, 'PictureURL', :optional => true
+      array_node :picture_urls, 'PictureURL', :optional => true, :unmarshaller => proc { |el| el.text }
       text_node :picture_source, 'PictureSource', :optional => true
       text_node :gallery_duration, 'GalleryDuration', :optional => true
       text_node :gallery_status, 'GalleryStatus', :optional => true
--- lib/ebay/types/variation_specific_picture_set.rb
+++ lib/ebay/types/variation_specific_picture_set.rb
@@ -10,7 +10,7 @@ module Ebay # :nodoc:
       include Initializer
       root_element_name 'VariationSpecificPictureSet'
       text_node :variation_specific_value, 'VariationSpecificValue', :optional => true
-      text_node :picture_url, 'PictureURL', :optional => true
+      array_node :picture_urls, 'PictureURL', :optional => true, :unmarshaller => proc { |el| el.text }
       text_node :gallery_url, 'GalleryURL', :optional => true
     end
   end
